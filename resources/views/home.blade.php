641cc1398aa6dfd38d9b5ea9473ef5261df75836
MikeEm <michalbanaszek03@gmail.com>
2025-01-12 21:17:25 +0100

    Lublin site resources

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content="Michał Banaszek"/>
+    <title>Lublin - miasto inspiracji</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico"/>
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Simple line icons-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css"
+          rel="stylesheet"/>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic"
+          rel="stylesheet" type="text/css"/>
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet"/>
+</head>
+<body id="page-top">
+<!-- Navigation-->
+<a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
+<nav id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <li class="sidebar-brand"><a href="#page-top">Lublin</a></li>
+        <li class="sidebar-nav-item"><a href="#page-top">Strona główna</a></li>
+        <li class="sidebar-nav-item"><a href="#about">Opis</a></li>
+        <li class="sidebar-nav-item"><a href="#whyUs">Nasze usługi</a></li>
+        <li class="sidebar-nav-item"><a href="#categories">Kategorie</a></li>
+        <li class="sidebar-nav-item"><a href="#opinions">Opinie</a></li>
+    </ul>
+</nav>
+<!-- Header-->
+<header class="masthead d-flex align-items-center">
+    <div class="container px-4 px-lg-5 text-center">
+        <h1 class="mb-1" style="text-shadow: -1px 0 silver, 0 1px silver, 1px 0 silver, 0 -1px silver;">Lublin</h1>
+        <h3 class="mb-5" style="text-shadow: -1px 0 silver, 0 1px silver, 1px 0 silver, 0 -1px silver;"><em>miasto
+                inspiracji</em></h3>
+        <a class="btn btn-primary btn-xl" href="#about">Sprawdź!</a>
+    </div>
+</header>
+<!-- About-->
+<section class="content-section bg-light" id="about">
+    <div class="container px-4 px-lg-5 text-center">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-auto">
+                <h2>Odkryj uroki Lublina - przewodnik po najciekawszych atrakcjach miasta!</h2>
+                <p class="lead mb-5">
+                    Witamy na stronie poświęconej Lublinowi, urokliwemu miastu położonemu we wschodniej Polsce, pełnemu
+                    bogatej historii, kultury i niezapomnianych miejsc. Lublin zachwyca swoją średniowieczną starówką,
+                    tętniącym życiem kulturalnym oraz wyjątkowym klimatem, który przyciąga turystów z całego świata. Na
+                    naszej stronie znajdziesz szczegółowe opisy najważniejszych atrakcji, praktyczne porady oraz
+                    propozycje tras zwiedzania, które pozwolą Ci odkryć to niezwykłe miasto w pełni. Zapraszamy do
+                    wspólnej podróży po Lublinie, mieście, które z pewnością zostanie w Twojej pamięci na długo.
+                </p>
+                <a class="btn btn-dark btn-xl" href="#categories">Co warto zobaczyć?</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- What to see?-->
+<section class="content-section bg-primary text-white text-center" id="whyUs">
+    <div class="container px-4 px-lg-5">
+        <div class="content-section-heading">
+            <h2 class="mb-5">Dlaczego powinieneś skorzystać z naszych usług?</h2>
+            <!--            <h3 class="text-secondary mb-3">lorem ipsum</h3>-->
+        </div>
+        <div class="row gx-4 gx-lg-5">
+            <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+                <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-screen-smartphone"></i></span>
+                <h4><strong>Uniwersalność</strong></h4>
+                <p class="text-faded mb-0">Dostępne na każdej platformie</p>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+                <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-reload"></i></span>
+                <h4><strong>Zawsze na czasie!</strong></h4>
+                <p class="text-faded mb-0">Stale aktualizowane informacje</p>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+                <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-check"></i></span>
+                <h4><strong>Gwarancja jakości</strong></h4>
+                <p class="text-faded mb-0">
+                    Sprawdzone propozycje
+                    <!--                    <i class="fas fa-heart"></i>-->
+                </p>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <span class="service-icon rounded-circle mx-auto mb-3"><i class="icon-question"></i></span>
+                <h4><strong>???</strong></h4>
+                <p class="text-faded mb-0">Odpowiedź na każde twoje pytanie</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Callout-->
+<section class="callout">
+    <div class="container px-4 px-lg-5 text-center">
+        <h2 class="mx-auto mb-5" style="text-shadow: -2px 0 white, 0 2px white, 2px 0 white, 0 -2px white;">
+            Zaplanuj swoją <em>podróż do Lublina</em> z nami!<br>
+            Zobacz jak dojechać do serca Lubelszczyzny
+        </h2>
+        <a class="btn btn-primary btn-xl"
+           href="https://www.google.com/maps/dir//Lublin/@51.2182631,22.4812641,12z/data=!4m9!4m8!1m0!1m5!1m1!1s0x472257a56cb59693:0x64c6cec2d5837066!2m2!1d22.5747275!2d51.2509458!3e0?entry=ttu"
+           target="_blank">W drogę!</a>
+    </div>
+</section>
+<!-- Portfolio-->
+<section class="content-section" style="padding-top: 3rem;" id="categories">
+    <div class="container px-4 px-lg-5">
+        <div class="content-section-heading text-center">
+            <h3 class="text-secondary mb-0">Kategorie</h3>
+            <h2 class="mb-5">Czego szukasz?</h2>
+        </div>
+        <div class="row gx-0">
+            <div class="col-lg-6">
+                <a class="portfolio-item" href="#!">
+                    <div class="caption">
+                        <div class="caption-content">
+                            <div class="h1 mb-1">Turystyka</div>
+                            <p class="mb-0">Turystyka w Lublinie oferuje bogactwo zabytków, festiwali kulturalnych i
+                                malowniczych krajobrazów, które zachwycają każdego odwiedzającego.
+                            </p>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="assets/img/tourism3_2.jpg" alt="..."/>
+                </a>
+            </div>
+            <div class="col-lg-6">
+                <a class="portfolio-item" href="food.html">
+                    <div class="caption">
+                        <div class="caption-content">
+                            <div class="h1 mb-1">Jedzenie</div>
+                            <p class="mb-0">Kuchnia Lublina zachwyca lokalnymi specjałami, takimi jak cebularze, pierogi
+                                oraz tradycyjne dania kuchni kresowej, które są prawdziwą ucztą dla smakoszy.
+                            </p>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="assets/img/niemiec3_2.jpg" alt="..."/>
+                </a>
+            </div>
+            <div class="col-lg-6">
+                <a class="portfolio-item" href="#!">
+                    <div class="caption">
+                        <div class="caption-content">
+                            <div class="h1 mb-1">Transport</div>
+                            <p class="mb-0">Transport w Lublinie jest dobrze zorganizowany, obejmując rozbudowaną sieć
+                                komunikacji miejskiej, która ułatwia podróżowanie po całym mieście i jego okolicach.
+                            </p>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="assets/img/dworzec3_2.jpg" alt="..."/>
+                </a>
+            </div>
+            <div class="col-lg-6">
+                <a class="portfolio-item" href="#!">
+                    <div class="caption">
+                        <div class="caption-content">
+                            <div class="h1 mb-1">Sport</div>
+                            <p class="mb-0">Sport w Lublinie kwitnie dzięki licznym klubom i obiektom sportowym, gdzie
+                                mieszkańcy i turyści mogą aktywnie spędzać czas, uprawiając różnorodne dyscypliny.
+                            </p>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="assets/img/arena3_2.jpg" alt="..."/>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Call to Action-->
+<!--<section class="content-section bg-primary text-white">-->
+<!--    <div class="container px-4 px-lg-5 text-center">-->
+<!--        <h2 class="mb-4">The buttons below are impossible to resist...</h2>-->
+<!--        <a class="btn btn-xl btn-light me-4" href="#!">Click Me!</a>-->
+<!--        <a class="btn btn-xl btn-dark" href="#!">Look at Me!</a>-->
+<!--    </div>-->
+<!--</section>-->
+<!-- Map-->
+<!--<div class="map" id="contact">-->
+<!--    <iframe src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>-->
+<!--    <br/>-->
+<!--    <small><a-->
+<!--            href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a></small>-->
+<!--</div>-->
+
+<!-- Bootstrap 5 Contact Form Snippet -->
+<section class="content-section bg-primary" id="opinions">
+    <div class="container px-5 my-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="card border-0 rounded-3 shadow-lg">
+                    <div class="card-body p-4">
+                        <div class="text-center">
+                            <div class="h1 fw-light">Zostaw opinię</div>
+                            <p class="mb-4 text-muted">Spodobała ci się nasza strona? A może masz jakieś uwagi i
+                                chciałbyś się nimi podzielić? Zostaw nam opinię!</p>
+                        </div>
+                        <form id="contactForm">
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="name" type="text" placeholder="Name"
+                                       pattern="^[a-zA-ZąćęłńóśżźĄĆĘŁŃÓŚŻŹ]{2,20}$" maxlength="20"
+                                       required/>
+                                <label for="name">Nazwa</label>
+                                <div class="invalid-feedback">Nazwa jest wymagana.</div>
+                            </div>
+
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="emailAddress" type="email" placeholder="Email"
+                                       maxlength="30" size="30"
+                                       pattern="^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
+                                <label for="emailAddress">Adres email</label>
+                                <div class="invalid-feedback">Adres email jest wymagany.
+                                </div>
+                                <div class="invalid-feedback">Adres email jest niepoprawny.
+                                </div>
+                            </div>
+                            <!--                            <div class="form-floating mb-3">-->
+                            <!--                                <input class="form-control" id="artist" type="text" placeholder="Artist"-->
+                            <!--                                       maxlength="20" required/>-->
+                            <!--                                <label for="artist">Artist's Name</label>-->
+                            <!--                                <div class="invalid-feedback">Artist's name is-->
+                            <!--                                    required.-->
+                            <!--                                </div>-->
+                            <!--                            </div>-->
+                            <div class="form-floating mb-3">
+                                <textarea class="form-control" id="message" type="text"
+                                          placeholder="Napisz swoją opinię" style="height:7rem;"
+                                          required></textarea>
+                                <label for="message">Napisz swoją opinię...</label>
+                                <div class="invalid-feedback">Nie napisałeś nam opnii.. :c
+                                </div>
+                            </div>
+                            <p class="mb-1 text-muted" style="text-align: center">Jak bardzo zadowolony jesteś z naszych usług(1-5)?</p>
+                            <!--                            <div class="checkbox-container"-->
+                            <!--                                 style="display: flex;justify-content: center; gap: 5px; margin-bottom: 0.5rem">-->
+                            <!--                                <input type="checkbox" name="spotify" id="spotify" checked>-->
+                            <!--                                <label for="spotify">Spotify</label>-->
+                            <!--                                <input type="checkbox" name="Youtube" id="Youtube">-->
+                            <!--                                <label for="Youtube">Youtube</label>-->
+                            <!--                                <input type="checkbox" name="AppleMusic" id="AppleMusic">-->
+                            <!--                                <label for="AppleMusic">Apple Music</label>-->
+                            <!--                                <input type="checkbox" name="Tidal" id="Tidal">-->
+                            <!--                                <label for="Tidal">Tidal</label>-->
+                            <!--                            </div>-->
+                            <div class="radio-container"
+                                 style="display: flex; justify-content: center; gap: 8px; margin: 10px">
+                                <input type="radio" name="rating" id="star1" value="1" required>
+                                <label for="star1">1 </label>
+                                <input type="radio" name="rating" id="star2" value="2" required>
+                                <label for="star2">2 </label>
+                                <input type="radio" name="rating" id="star3" value="3" required>
+                                <label for="star3">3 </label>
+                                <input type="radio" name="rating" id="star4" value="4" required>
+                                <label for="star4">4 </label>
+                                <input type="radio" name="rating" id="star5" value="5" required>
+                                <label for="star5">5 </label>
+                            </div>
+                            <div class="d-none" id="submitSuccessMessage">
+                                <div class="text-center mb-3">
+                                    <div class="fw-bolder">Form submission successful!</div>
+                                </div>
+                            </div>
+                            <div class="d-none" id="submitErrorMessage">
+                                <div class="text-center text-danger mb-3">Error sending message!</div>
+                            </div>
+
+                            <div class="d-grid">
+                                <button class="btn btn-primary btn-lg" id="submitButton" type="submit">Wystaw opinię
+                                </button>
+                            </div>
+                        </form>
+                        <div id="form-error"></div>
+                        <section id="data-section">
+                            <h2 class="fw-light" style="margin-top: 1em">Opinie użytkowników:</h2>
+                            <ul id="data-list"></ul>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- CDN Link to SB Forms Scripts -->
+<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+<!-- Footer-->
+<footer class="footer text-center">
+    <div class="container px-4 px-lg-5">
+        <ul class="list-inline mb-5">
+            <li class="list-inline-item">
+                <a class="social-link rounded-circle text-white mr-3" href="https://www.facebook.com/MiastoLublin/"
+                   target="_blank"><i class="icon-social-facebook"></i></a>
+            </li>
+            <li class="list-inline-item">
+                <a class="social-link rounded-circle text-white mr-3" href="https://x.com/miasto_lublin?lang=pl"
+                   target="_blank"><i class="icon-social-twitter"></i></a>
+            </li>
+            <li class="list-inline-item">
+                <a class="social-link rounded-circle text-white" href="https://www.instagram.com/miastolublin/"
+                   target="_blank"><i class="icon-social-instagram"></i></a>
+            </li>
+        </ul>
+        <p class="text-muted small mb-0"> Lublin - miasto inspiracji <br>Copyright &copy MB</p>
+    </div>
+</footer>
+<!-- Scroll to Top Button-->
+<script src="js/local.js"></script>
+<a class="scroll-to-top rounded" href="#page-top"><i class="fas fa-angle-up"></i></a>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+</body>
+</html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
