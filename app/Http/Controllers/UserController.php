07dd380148e4807a8ca9c4c91c5a27d725891a16
MikeEm <michalbanaszek03@gmail.com>
2025-01-12 18:07:21 +0100

    register field server validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,9 +9,9 @@ class UserController extends Controller
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
-            'name' => 'required|string',
+            'name' => 'required'|'string'|'min:3'|'max:20',
             'email' => 'required|email',
-            'password' => 'required|string|min:6'
+            'password' => 'required|string|min:7|max:20',
         ]);
         return 'Hello from our controller';
     }
