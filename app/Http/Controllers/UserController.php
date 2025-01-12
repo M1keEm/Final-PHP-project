7295859bde16716e34cee3012e5f7081e322a2bd
MikeEm <michalbanaszek03@gmail.com>
2025-01-12 18:25:34 +0100

    user registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,10 +10,12 @@ class UserController extends Controller
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
-            'name' => 'required'|'string'|'min:3'|'max:20',
+            'name' => 'required|string|min:3|max:20',
             'email' => 'required|email',
             'password' => 'required|string|min:7|max:20',
         ]);
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        User::create($incomingFields);
         return 'Hello from our controller';
     }
 }
