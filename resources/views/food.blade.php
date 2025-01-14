289bb2e74e2d2f83a8eeea76c087110dce1bb9da
MikeEm <michalbanaszek03@gmail.com>
2025-01-15 00:35:02 +0100

    smaller redundancy

--- resources/views/food.blade.php
+++ resources/views/food.blade.php
@@ -174,47 +174,6 @@
                 </div>
             </div>
         @endauth
-
-        <!-- Blog post-->
-        <!--                    <div class="card mb-4">-->
-        <!--                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"-->
-        <!--                                          alt="..."/></a>-->
-        <!--                        <div class="card-body">-->
-        <!--                            <div class="small text-muted">January 1, 2023</div>-->
-        <!--                            <h2 class="card-title h4">Post Title</h2>-->
-        <!--                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis-->
-        <!--                                aliquid atque, nulla.</p>-->
-        <!--                            <a class="btn btn-primary" href="#!">Read more →</a>-->
-        <!--                        </div>-->
-        <!--                    </div>-->
-        {{--                </div>--}}
-        {{--                <div class="col-lg-6">--}}
-        {{--                    <!-- Blog post-->--}}
-        {{--                    <div class="card mb-4">--}}
-        {{--                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"--}}
-        {{--                                          alt="..."/></a>--}}
-        {{--                        <div class="card-body">--}}
-        {{--                            <div class="small text-muted">27 maja, 2024</div>--}}
-        {{--                            <h2 class="card-title h4">Post Title</h2>--}}
-        {{--                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis--}}
-        {{--                                aliquid atque, nulla.</p>--}}
-        {{--                            <a class="btn btn-primary" href="#!">Read more →</a>--}}
-        {{--                        </div>--}}
-        {{--                    </div>--}}
-        <!-- Blog post-->
-        <!--                    <div class="card mb-4">-->
-        <!--                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"-->
-        <!--                                          alt="..."/></a>-->
-        <!--                        <div class="card-body">-->
-        <!--                            <div class="small text-muted">January 1, 2023</div>-->
-        <!--                            <h2 class="card-title h4">Post Title</h2>-->
-        <!--                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis-->
-        <!--                                aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam.</p>-->
-        <!--                            <a class="btn btn-primary" href="#!">Read more →</a>-->
-        <!--                        </div>-->
-        <!--                    </div>-->
-        {{--                </div>--}}
-        {{--            </div>--}}
         <!-- Pagination-->
         {{--            <nav aria-label="Pagination">--}}
         {{--                <hr class="my-0"/>--}}
@@ -245,38 +204,6 @@
         {{--                                    </div>--}}
         {{--                                </div>--}}
         {{--                            </div>--}}
-        {{--            <!-- Categories widget-->--}}
-        {{--            <div class="card mb-4">--}}
-        {{--                <div class="card-header">Categories</div>--}}
-        {{--                <div class="card-body">--}}
-        {{--                    <div class="row">--}}
-        {{--                        <div class="col-sm-6">--}}
-        {{--                            <ul class="list-unstyled mb-0">--}}
-        {{--                                <li><a href="#!">Web Design</a></li>--}}
-        {{--                                <li><a href="#!">HTML</a></li>--}}
-        {{--                                <li><a href="#!">Freebies</a></li>--}}
-        {{--                            </ul>--}}
-        {{--                        </div>--}}
-        {{--                        <div class="col-sm-6">--}}
-        {{--                            <ul class="list-unstyled mb-0">--}}
-        {{--                                <li><a href="#!">JavaScript</a></li>--}}
-        {{--                                <li><a href="#!">CSS</a></li>--}}
-        {{--                                <li><a href="#!">Tutorials</a></li>--}}
-        {{--                            </ul>--}}
-        {{--                        </div>--}}
-        {{--                    </div>--}}
-        {{--                </div>--}}
-        {{--            </div>--}}
-        <!-- Side widget-->
-        {{--            <div class="card mb-4">--}}
-        {{--                <div class="card-header">Side Widget</div>--}}
-        {{--                <div class="card-body">You can put anything you want inside of these side widgets. They are easy to use,--}}
-        {{--                    and feature the Bootstrap 5 card component!--}}
-        {{--                </div>--}}
-        {{--            </div>--}}
-        {{--    </div>--}}
-        {{--</div>--}}
-        {{--</div>--}}
     </div>
 </section>
 <!-- CDN Link to SB Forms Scripts -->
